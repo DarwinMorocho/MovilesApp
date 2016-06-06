25a2128d45296c07a8287c80e9dac2dfc4b6c732
Chucho1991 <chucho17_jesus@hotmail.com>
2016-06-06 13:20:34 -0500

    cambiosJesus3

--- app/src/main/java/ec/edu/com/epn/konwarriosapp/Generos.java
+++ app/src/main/java/ec/edu/com/epn/konwarriosapp/Generos.java
@@ -1,13 +1,26 @@
 package ec.edu.com.epn.konwarriosapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 public class Generos extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_generos);
+        setContentView(R.layout.activity_generos2);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
+
+    public void abrirCrearGenero(View view){
+        Intent llamadoCrear = new Intent(this, CrearNuevoGenero.class);
+        startActivity(llamadoCrear);
+    }
+
 }
