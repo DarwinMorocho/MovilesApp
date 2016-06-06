ea755fe13c22f65858bd266ecf23155f021d7aa6
Chucho1991 <chucho17_jesus@hotmail.com>
2016-06-06 12:58:05 -0500

    cambiosJesus

--- app/src/main/java/ec/edu/com/epn/konwarriosapp/Artistas.java
+++ app/src/main/java/ec/edu/com/epn/konwarriosapp/Artistas.java
@@ -1,13 +1,26 @@
 package ec.edu.com.epn.konwarriosapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 public class Artistas extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_artistas);
+        setContentView(R.layout.activity_artistas2);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
+
+    public void abrirCrearArtista(View view){
+        Intent llamadoCrear = new Intent(this, CrearNuevoArtista.class);
+        startActivity(llamadoCrear);
+    }
+
 }
