b946576ae60925eec3b4606d6543b064c4ff75af
Chucho1991 <chucho17_jesus@hotmail.com>
2016-06-06 13:07:44 -0500

    cambiosJesus2

--- app/src/main/java/ec/edu/com/epn/konwarriosapp/Canciones.java
+++ app/src/main/java/ec/edu/com/epn/konwarriosapp/Canciones.java
@@ -1,13 +1,35 @@
 package ec.edu.com.epn.konwarriosapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 public class Canciones extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_canciones);
+        setContentView(R.layout.activity_canciones2);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
     }
+
+    public void abrirCrearCancion(View view){
+        Intent llamadoCrear = new Intent(this, CrearNuevaCancion.class);
+        startActivity(llamadoCrear);
+    }
+
 }
---
+++ app/src/main/java/ec/edu/com/epn/konwarriosapp/CrearNuevaCancion.java
@@ -0,0 +1,13 @@
+package ec.edu.com.epn.konwarriosapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CrearNuevaCancion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crear_nueva_cancion);
+    }
+}
