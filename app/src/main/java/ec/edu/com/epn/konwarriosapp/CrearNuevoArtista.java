5268628d1923c6c5acb2f4940e6cba2ebd77be17
DarwinGato1411 <darwinvinicio14_11@hotmail.com>
2016-06-06 12:01:32 -0500

    mi primer commit

---
+++ app/src/main/java/ec/edu/com/epn/konwarriosapp/CrearNuevoArtista.java
@@ -0,0 +1,13 @@
+package ec.edu.com.epn.konwarriosapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CrearNuevoArtista extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crear_nuevo_artista);
+    }
+}
